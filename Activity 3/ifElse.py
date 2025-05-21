b4a273a70578a3e213ffbf9b7627e9afe0e70be6
KangKongKen <mojicakennpaul@gmail.com>
2025-05-22 00:11:18 +0800

    Delete Activity 3 directory

--- Activity 3/ifElse.py
+++
@@ -1,6 +0,0 @@
-temperature = int(input("Enter today's temperature in °C: "))
-
-if temperature >= 35:
-    print("Warning: Heatwave alert!")
-else:
-    print("Temperature is normal today.")
--- Activity 3/switchCase.py
+++
@@ -1,9 +0,0 @@
-day = input("Enter a day: ").lower()
-
-match day:
-    case "saturday" | "sunday":
-        print("It's the weekend! Relax time.")
-    case "monday" | "tuesday" | "wednesday" | "thursday" | "friday":
-        print("It's a workday. Stay productive!")
-    case _:
-        print("That's not a valid day.")
